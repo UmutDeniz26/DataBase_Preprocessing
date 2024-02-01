59ffb1d7980e9c02c45797e1408bbd34f2aa0d15
UmutDeniz26 <umutcandeniz2001@gmail.com>
2024-02-01 14:22:03 +0300

    Minor update about writeToTxtpy

--- UMUT/writeToTxt.py
+++ UMUT/writeToTxt.py
@@ -3,6 +3,11 @@ def run(txt_path, resp):
     new_dict = {key: value for key, value in resp.items() if key not in ['distance_nose_left_eye', 'distance_nose_right_eye', 'difference_between_le_re']}
     keys = list(new_dict.keys())
 
+    if txt_path.endswith(".bmp"):
+        txt_path = txt_path.replace(".bmp", ".txt")
+    if txt_path.endswith(".jpg"):
+        txt_path = txt_path.replace(".jpg", ".txt")
+
     #txt file operations
     os.makedirs(os.path.dirname(txt_path), exist_ok=True)
 
