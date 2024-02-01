57c127b079b426fba9cc985d73b8cfbf2585a785
Oksuzali <oksuzali09@gmail.com>
2024-02-01 11:39:52 +0300

    detect_distences_of_sides scriptinin dönüt olarak verdiği resul_dict içerisine retina_face'in verdiği değişkenler eklendi. Retina face'in yüz bulamama durumu esnasında kodun hata vermemesi için if else yapısı oluşturuldu.

--- Ali/run.py
+++ Ali/run.py
@@ -1,8 +1,8 @@
 from detect_frontal_face import copy_max_abs_image_to_folder
 import os
 
-source_folder = "CASIA-FaceV5(BMP)/Face/"
-destination_folder = "CASIA-FaceV5(BMP)/Face/"
+source_folder = "./Ali/CASIA-FaceV5(BMP)/Face/"
+destination_folder = "./Ali/CASIA-FaceV5(BMP)/Face"
 
 for filename in os.listdir(source_folder):
 
@@ -10,4 +10,4 @@ for filename in os.listdir(source_folder):
     image_path = os.path.join(source_folder, filename)
 
     # copy_max_abs_image_to_folder fonksiyonunu çağır
-    copy_max_abs_image_to_folder(image_path, destination_folder)
+    copy_max_abs_image_to_folder(image_path, image_path)
