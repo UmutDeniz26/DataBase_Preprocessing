5e20551f54f5e99e5a2601b1245d441714501af4
elifozkann <elifbeyzaozkan51@gmail.com>
2024-02-05 11:50:23 +0300

    Her klasörde max 200 img olacak şekilde veri seti New_YoutubeFace şekilnde güncellendi

--- Elif/YoutubeFace/run.py
+++ Elif/YoutubeFace/run.py
@@ -4,6 +4,6 @@ sys.path.insert(0,"./UMUT")
 
 import DB_Folder_Manipulator
 
-DB_Folder_Manipulator.main(dbName='YoutubeFace', upperFolderName='Elif', False, False,False )
+DB_Folder_Manipulator.main(dbName='New_YotubeFace', upperFolderName='Elif', False, False,False )
 
 
