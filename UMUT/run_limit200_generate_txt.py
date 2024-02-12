c7cfd5d8c38ba0161f30a0d7c29e34aa933f8da4
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-02-12 21:07:51 +0300

    modular listing tools

--- UMUT/run_limit200_generate_txt.py
+++ UMUT/run_limit200_generate_txt.py
@@ -1,3 +1,11 @@
 import sys
 sys.path.insert(0,'Elif/YoutubeFace')
 import img_sayısı_200
+import listeleme_sirali
+
+dataset_path="./UMUT/YoutubeFace"
+output_file='./UMUT/new.txt'
+output_folder = './UMUT/YoutubeFace200'
+
+img_sayısı_200.main(dataset_path,output_folder)
+listeleme_sirali.main(output_folder, output_file)
