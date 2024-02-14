9066c82e9be37f08db1cb73810f47681499fff8e
Oksuzali <oksuzali09@gmail.com>
2024-02-14 12:57:51 +0300

    commit

--- Ali/arc.py
+++ Ali/arc.py
@@ -1,1 +1,33 @@
-import sys
+import os
+
+def main(dataset_path, output_file):
+    counter = 0
+    n = 0
+
+    with open(output_file, 'w') as file:
+        # Ana dizindeki klasörleri listele
+        for main_folder in sorted(os.listdir(dataset_path)):
+            main_folder_path = os.path.join(dataset_path, main_folder)
+
+            # Klasörleri kontrol et ve içerisindeki dosyaları listele
+            if os.path.isdir(main_folder_path):
+                # Person'a ait tüm alt klasörlerdeki görselleri listele
+                for subfolder_name in sorted(os.listdir(main_folder_path)):
+                    subfolder_path = os.path.join(main_folder_path, subfolder_name)
+
+                    if os.path.isdir(subfolder_path):
+                        # Alt klasördeki dosyaları listele
+                        for image_file in sorted(os.listdir(subfolder_path)):
+                            person_number = f'{counter:04d}'
+                            img_number = f'{n:06d}'
+
+                            file.write(f'{person_number}_{subfolder_name}_{img_number}\n')
+                            n += 1
+
+                counter += 1
+
+    print(f'Liste oluşturuldu ve {output_file} adında kaydedildi.')
+
+if __name__ == "__main__":
+    main(dataset_path="./Ali/YoutubeFace",
+         output_file='new.txt')
--- Ali/run.py
+++ Ali/run.py
@@ -14,7 +14,9 @@ sys.path.insert(0, "./UMUT")
 
 import DB_Folder_Manipulator
 
-DB_Folder_Manipulator.main(dbName='CASIA-FaceV5(BMP)', upperFolderName='Ali',
-    inputOrAutoMod=False, printFeaturesFlag=False,
-        selectFirstImageAsFrontal=False, showAlignedImages=False,
-        alignImagesFlag=True, resetImagesFlag=False) #if resetImagesFlag is True, then the images will be recreated
+DB_Folder_Manipulator.main(
+        data_base_name='YoutubeFace', upper_folder_name='Ali',
+        align_images_flag=True, reset_images_flag=False,
+        auto_feature_select=False, print_features_flag=False,
+        select_first_image_as_frontal=False, show_aligned_images=False
+    )
