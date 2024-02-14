535d88f656fb5cee311fa57dda756d3c8887b7a7
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-02-14 12:01:04 +0300

    printing changes

--- UMUT/ConcatFolders/concat.py
+++ UMUT/ConcatFolders/concat.py
@@ -16,25 +16,24 @@ def main(output_folder_path):
         persons = return_files(data_base_part.path)
         for person in persons:
             if person.name == 'Frontal_Faces':
+                print(f'{person.name} is frontal folder')
                 continue
             if person.is_file():
+                print(f'{person.name} is a file')
                 continue
             intras = return_files(person.path)
             for intra in intras:
                 inters = return_files(intra.path)
                 for inter in inters:
+                    os.makedirs(f'{output_folder_path}/{person_id:08d}/{intra_id:08d}', exist_ok=True)
                     if inter.is_file():
-                        os.makedirs(f'{output_folder_path}/{person_id:08d}/{intra_id:08d}', exist_ok=True)
-                        print(inter.path)
+                        # Copy file to output
                         extention = inter.name.split('.')[-1]
                         shutil.copy(inter.path, f'{output_folder_path}/{person_id:08d}/{intra_id:08d}/{inter_id:08d}.{extention}')
                         inter_id += 1
                     if  inter.is_dir():
-                        os.makedirs(f'{output_folder_path}/{person_id:08d}/{intra_id:08d}', exist_ok=True)
-                        print(inter.path)
                         # Copy frontal folder to output
                         shutil.copytree(inter.path, f'{output_folder_path}/{person_id:08d}/{intra_id:08d}/Frontal')
-
                 intra_id += 1
             person_id += 1
 if __name__ == '__main__':
