6eac0be53509be938871242d0e4cf152646b5014
ipekbulgurcu <ipekblgrc@gmail.com>
2024-02-15 10:17:57 +0300

    change

--- IPEK/runDB.py
+++ IPEK/runDB.py
@@ -6,7 +6,7 @@ import DB_Folder_Manipulator
 
 DB_Folder_Manipulator.main(
         data_base_name='LFW', upper_folder_name='IPEK',
-        align_images_flag=True, reset_images_flag=True,
+        align_images_flag=True, reset_images_flag=False,
         auto_feature_select=False, print_features_flag=False,
         select_first_image_as_frontal=False, show_aligned_images=False
     )
