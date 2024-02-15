f3c731b1c492714834652d329e9f86993f70896f
elifozkann <elifbeyzaozkan51@gmail.com>
2024-02-15 10:27:57 +0300

    before pull

--- Elif/YoutubeFace/run.py
+++ Elif/YoutubeFace/run.py
@@ -6,7 +6,7 @@ import DB_Folder_Manipulator
 
 DB_Folder_Manipulator. main(
         data_base_name='YoutubeFace', upper_folder_name='Elif',
-        align_images_flag=True, reset_images_flag=True,
+        align_images_flag=True, reset_images_flag=False,
         auto_feature_select=False, print_features_flag=False,
         select_first_image_as_frontal=False, show_aligned_images=False
     )
