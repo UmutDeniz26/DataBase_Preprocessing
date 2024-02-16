ea18b3eb4082463d216d418d3efd30fe28414dec
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-02-16 10:38:34 +0300

    main txt writer changes

--- UMUT/main_txt_writer.py
+++ UMUT/main_txt_writer.py
@@ -4,11 +4,8 @@ import txtFileOperations
 def main(folder_path,data_base_name,upper_folder_name):
     print(txtFileOperations.initMainTxtFile(data_base_name,upper_folder_name,
         ["file_path","inter","intra","right_eye","left_eye","nose","mouth_right","mouth_left"]))
-
-
     inter = 0
     intra = 0
-
     for root , dirs, files in os.walk(folder_path):
         if len(files) > 1:
             for file in files:
@@ -18,9 +15,6 @@ def main(folder_path,data_base_name,upper_folder_name):
                     txtFileOperations.writeFileMainTxt(os.path.join(root,file), resp_dict, inter, intra)
                     intra+=1
             inter+=1
-
-
-
 
 if __name__ == "__main__":
     main(folder_path="UMUT/AFW_FOLDERED",data_base_name="AFW",upper_folder_name="UMUT")
