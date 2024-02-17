fb1717cd05eac3addd84bda1cc724e66f6000793
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-02-17 18:46:45 +0300

    update error_handle

--- UMUT/Error_Handle/error_handle.py
+++ UMUT/Error_Handle/error_handle.py
@@ -11,24 +11,79 @@ def count_path_slices(path):
     return len(slices)
 
 def main(folder_path, delete_all_errors=False):
-
     large_folder_flag = False
 
-    for root, dirs, files in os.walk(folder_path):
-        if len(files) < 100:
-            large_folder_flag = True
-        else:
-            large_folder_flag = False
+    total_folder_count = 0;index = 0;error_count = 0
+    error_info_paths = [];error_info = [];deleted_files = []
 
-        print("Root: ", root)
+    for root, dirs, files in os.walk(folder_path):
+        total_folder_count += 1
+
+    for root, dirs, files in os.walk(folder_path):
+        percentage = (index/total_folder_count)*100
+        print(f"\n\n\nProgress: {percentage:.2f}%", end="\r")
+        #os.system('cls')
+        index+=1
+
+        error_count = 0
         if count_path_slices(root) > 4:
+            temp_error_files = []
             for file in files:
                 if file.endswith(".txt"):
+                    txt_path = os.path.join(root, file)
+                    img_path = txt_path.replace(".txt", ".jpg")
                     content = open(os.path.join(root, file)).read()
-                    if delete_all_errors:
-                        print("File path: ", os.path.join(root, file))
-                        print("Content: ", content)
+                    if "Error" in content or "Stack Overflow" in content or "too small" in content or "LandmarkError" in content:
+                        if delete_all_errors:
+                            temp_error_files.append(img_path)
+                            temp_error_files.append(txt_path)
+                            error_count += 1
+            ratio = error_count/ (len(files)/2)
+            correct_files = len(files)/2 - error_count
+            if correct_files < 10:
+                string = (f"Error count: {error_count}  "
+                          f"Total files: {len(files)}  "
+                          f"Ratio: {ratio}  "
+                          f"Correct files: {int(correct_files)}  "
+                          f"Folder path: {root}")
+                #print(string)
+                error_info.append(string)
+                for file in temp_error_files:
+                    error_info_paths.append(file)
+                    
+            else:
+                for file in files:
+                    if file.endswith(".txt"):
+                        txt_path = os.path.join(root, file)
+                        img_path = txt_path.replace(".txt", ".jpg")
+                        with open(txt_path, "r") as f:
+                            content = f.read()
+
+                        if "Error" in content or "Stack Overflow" in content or "too small" in content or "LandmarkError" in content:
+                            if delete_all_errors:
+                                if os.path.exists(img_path) and os.path.exists(txt_path):
+                                    os.remove(img_path)
+                                    deleted_files.append(img_path)
+                                    os.remove(txt_path)
+                                    deleted_files.append(txt_path)
+                                else:
+                                    print(f"Files does not exist: {img_path}")
+                                    exit()
+
+    with open("deleted_files.txt", "w") as f:
+        for item in deleted_files:
+            f.write("%s\n" % item)
     
+    write_txt_path = os.path.join("UMUT","Error_Handle", "error.txt")
+    with open(write_txt_path, "w") as f:
+        for item in error_info:
+            f.write("%s\n" % item)
+    
+    write_txt_path = os.path.join("UMUT","Error_Handle", "error_paths.txt")
+    with open(write_txt_path, "w") as f:
+        for item in error_info_paths:
+            f.write("%s\n" % item)
+            
 
 if __name__ == "__main__":
-    main(folder_path="UMUT\Two_Face_Handle\HELEN_FOLDERED_copy", delete_all_errors=True)
+    main(folder_path="UMUT\Two_Face_Handle\Output_copy", delete_all_errors=True)
