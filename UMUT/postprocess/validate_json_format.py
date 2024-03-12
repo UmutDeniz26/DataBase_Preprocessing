0a531e5145b9a607d544845a1bf1333738721b0e
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-03-12 10:47:12 +0300

    Refactor validate_json_format.py to handle JSON errors and print content for debugging

--- UMUT/postprocess/validate_json_format.py
+++ UMUT/postprocess/validate_json_format.py
@@ -1,38 +1,54 @@
 import os
 import json
 
-def main(folder_path):
+def main(folder_path, fix_json=False, print_flag=False):
     
     for root, dirs, files in os.walk(folder_path):
         for file in files:
-            if file.endswith(".txt"):
+            if file.endswith(".txt") and "_Info" not in file:
+                # Get the content of the txt file
                 txt_path = os.path.join(root, file)
-                with open(txt_path, "r") as f:
-                    content = f.read()
-                if "pass_exception" in content or "info" in file.lower():
-                    continue
+                content = get_content(txt_path)
 
                 error_flag = False
                 try:
-                    
-                    json_data = json.loads(content)
-                    error_flag = True if len(json_data) == 0 else False
-                    
+                    json_data = json.loads(content);json_data
                 except:
+                    if "Low Confidence" in content:
+                        content = content.replace("\"(Low Confidence)\",", "")
+                    content = content.replace("(", "[").replace(")", "]")
                     error_flag = True
 
-                if error_flag:
-                    print(f"\nOld Content: {content}")
-                    content = f"{{\"Error\": \"JsonError\"}}"
-                    write_txt(txt_path, content)
-                    print(f"Fixed: {txt_path}")
+                if len(content)<200 and len(content)>25:
+                    print(f"\n{txt_path} - {len(content)}")
                     print(f"Content: {content}")
-                    
+                    print(f"Error: {error_flag}")
+                    input("Press Enter to continue...")
 
+                error_flag = True if len(json_data) == 0 else error_flag
+                error_flag = False if "pass_exception" in content else error_flag
+                if error_flag:
+                    if "right_eye" not in content:
+                        content = f"{{\"Error\": \"JsonError\"}}"
+                    if fix_json:
+                        write_txt(txt_path, content)
+                        print(f"\nFixed: {txt_path}")
+            
+                if print_flag:
+                    person_id = os.path.normpath(txt_path).split(os.sep)[-2]   
+                    if int(person_id) % 100 == 0:    
+                        print(f"\nFixed: {txt_path} - Error: {error_flag}")
+                        print(f"Content: {content}")
+            
+
+def get_content(txt_path):
+    with open(txt_path, "r") as f:
+        content = f.read()
+    return content
 
 def write_txt(txt_path, content):
     with open(txt_path, "w") as f:
         f.write(content)
 
 if __name__ == "__main__":
-    main("casia-webface_FOLDERED")
+    main("UMUT/casia-webface_FOLDERED", fix_json=False,print_flag=False)
