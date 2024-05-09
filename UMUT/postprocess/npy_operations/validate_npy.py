7ba383171bfff863ffebed523968f976e3a43fdc
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-05-09 08:32:58 +0300

    Refactor npy_operations/validate_npy.py and update data concatenation

--- UMUT/postprocess/npy_operations/validate_npy.py
+++ UMUT/postprocess/npy_operations/validate_npy.py
@@ -6,7 +6,7 @@ def main(npy_folder_path):
     #data_base_name = npy_folder_path.split("\\")[-1]
     #npy_folder_path = os.path.join(npy_folder_path, data_base_name + "_Info.npy")
     data = np.load(npy_folder_path)
-    #data2 = np.load("esogu_sets_meta_10_people.npy", allow_pickle=True)
+    data2 = np.load("src/others/esogu_sets_meta_10_people.npy", allow_pickle=True)
     print("\n\n\n",npy_folder_path)
     print(data.shape)
 
@@ -36,13 +36,18 @@ def main(npy_folder_path):
             print("Outer Path is not valid: ", path)
             exit()
     
-    """
     print(data2.shape)
     try:
         data_concat = np.concatenate((data, data2))
         print("Concatenation is successful")
     except:
         print("Concatenation is not successful")
+
+
+    print("5 examples from the data: ")
+    for i in range(5):
+        print(data[i])
+    """
     """
 if __name__ == '__main__':
     #main("UMUT/src/npy/YoutubeVideos_Info.npy")
