5d52f6cbcbfbc40209e80cb65b2f7dfe0c7d4228
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-05-09 08:59:36 +0300

    Refactor npy_operations/validate_npy.py for improved data validation and concatenation

--- UMUT/postprocess/npy_operations/validate_npy.py
+++ UMUT/postprocess/npy_operations/validate_npy.py
@@ -1,22 +1,21 @@
 import numpy as np
 import os
 import cv2
-def main(npy_folder_path):
-    #npy_folder_path = os.path.normpath(npy_folder_path)
-    #data_base_name = npy_folder_path.split("\\")[-1]
-    #npy_folder_path = os.path.join(npy_folder_path, data_base_name + "_Info.npy")
+def main(npy_folder_path,npy_concat_path=None) -> None:
+    """
+        Main function for the validation of the npy files.
+        
+        Args:
+            npy_folder_path (str): Path to the npy file
+            npy_concat_path (str): Path to the npy file to concatenate with the first one
+    """
     data = np.load(npy_folder_path)
-    data2 = np.load("src/others/esogu_sets_meta_10_people.npy", allow_pickle=True)
-    print("\n\n\n",npy_folder_path)
-    print(data.shape)
 
     for index,path in enumerate(data["mask"]):
-        if index%1000==0:
-            print("Counter: ",index," Path: ",path)
+        print("Counter: ",index," Path: ",path) if index%20000==0 else None
+        
         try:
-            if os.path.exists(path):
-                True
-            else:
+            if not os.path.exists(path):
                 print("Path is not valid: ", path)
                 exit()
         except:
@@ -24,37 +23,37 @@ def main(npy_folder_path):
             exit()
 
     for index,path in enumerate(data["path"]):
-        if index%1000==0:
-            print("Counter: ",index," Path: ",path)
+        print("Counter: ",index," Path: ",path) if index%20000==0 else None
+
         try:
-            if os.path.exists(path):
-                True
-            else:
+            if not os.path.exists(path):
                 print("Path is not valid: ", path)
                 exit()
         except:
             print("Outer Path is not valid: ", path)
             exit()
-    
-    print(data2.shape)
-    try:
-        data_concat = np.concatenate((data, data2))
-        print("Concatenation is successful")
-    except:
-        print("Concatenation is not successful")
 
+    print("\n")
+    if npy_concat_path is not None:
+        data2 = np.load( npy_concat_path, allow_pickle=True)
+        try:
+            data3 = np.concatenate((data, data2))
+            print("Concatenation is successful")
+        except:
+            print("Concatenation is not successful")
+        print("Data path: ", npy_folder_path)
+        print("Concated path: ", npy_concat_path)
+        print("Data shape: ", data.shape)
+        print("Concated data shape: ", data2.shape)
+        print("Output shape: ", data3.shape,"\n")
 
-    print("5 examples from the data: ")
-    for i in range(5):
+    print("10 examples from the data: ")
+    for i in range(0,300,30):
         print(data[i])
-    """
-    """
+
 if __name__ == '__main__':
-    #main("UMUT/src/npy/YoutubeVideos_Info.npy")
-    #main("UMUT/src/final_datasets/AFW/AFW_Info.npy")
-    #main("UMUT/src/final_datasets/CASIA-FaceV5_BMP_FOLDERED_Info/CASIA-FaceV5_BMP_FOLDERED_Info.npy")
-    #main("UMUT/src/final_datasets/HELEN_Info/HELEN_Info.npy")
-    #main("UMUT/src/final_datasets/LFW/LFW_Info.npy")
-    #main("UMUT/src/final_datasets/LFPW_Info/LFPW_Info.npy")
-    main("casia-webface/_Info.npy")
+    main(
+        npy_folder_path= "casia-webface/_Info.npy", 
+        npy_concat_path="src/others/esogu_sets_meta_10_people.npy"
+        )
     pass
